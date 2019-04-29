3f787fc909c6abb476e0766cc842bb9fdc819e67
PriSomeda <35231565+PriSomeda@users.noreply.github.com>
2019-04-29 16:16:57 -0400

    Baseline model - Unimproved
    Adapt system to unimproved genetic material (baseline model), which includes: fit of a simpler model of HDOM, and ratio calibration of BA and N modules.

--- Code/R/stand.SITE.r
+++ Code/R/stand.SITE.r
@@ -1,7 +1,7 @@
 #' Calculates dominant height for a plot.
 #'
 #' \code{stand.SITE} Calculates dominant height, based on site index (at reference age 25 year) and
-#' stand age for a plot considering silvicultural managements (i.e. site preparation, fertilization).
+#' stand age for a plot.
 #'
 #' @export
 #' @author Priscila Someda-Dias, Salvador A. Gezan
@@ -9,12 +9,6 @@
 #' @param HDOM      Numeric value of mean Dominant Height (ft).
 #' @param SI        Numeric value of Site Index (ft) (Dominant Height of the plot at age 25 years).
 #' @param AGE       Numeric value of stand Age (years).
-#' @param Z1        If fertilized at planting then Z1 = 1. Default is 0.
-#' @param Z2        If bedded (site preparation) then Z2 = 1. Default is 0.
-#' @param Z3        If herbicide was used then Z3 = 1. Default is 0.
-#' @param T1        If only Nitrogen fertilization at AGEFERT then T1 = 1. Default is 0.
-#' @param T2        If N and P fertilization at AGEFERT then T2 = 1. Default is 0.
-#' @param AGEFERT   Numeric value of stand age (in years) where fertilization is planned. Required if T1=1 or T2=1.
 #'
 #' @return A list containing the parameters:
 #' \itemize{
@@ -23,8 +17,10 @@
 #' }
 #'
 #' @references
-#' Gonzalez-Benecke et al. (2010) - Forest management effects on in situ and ex situ slash pine forest carbon balance
-#' Forest Ecology and Management, 260(5), 795-805; https://doi.org/10.1016/j.foreco.2010.05.038
+#' Refit for CFGRP FSBPdata of Chapman & Richards ()
+#'  Hdom1 = Hdom2 x ((1-exp(b x Age1))/(1-exp(b x Age2))) ^ c
+#' Anamorphic site index equation: Hdom = a x S x (1-exp(b x Age)) ^ c
+#'
 #'
 #' @examples
 #' # Example 1 - No fertilization
@@ -37,45 +33,33 @@
 #' stand.SITE(HDOM=54,AGE=18)$SI
 
 
-stand.SITE  <-  function(HDOM=NA, SI=NA, AGE=NA, Z1=0, Z2=0, Z3=0, T1=0, T2=0, AGEFERT=NA){
+stand.SITE  <-  function(HDOM=NA, SI=NA, AGE=NA){
 
   # Dominant Height Prediction
-  a1 <- 1.3679
-  a2 <- -0.07345
-  a3 <- 1.804
-  a4 <- 0.678
-  a5 <- 0.546
-  a6 <- 1.395
-  a7 <- -0.412
-  a8 <- -0.0691
+  # UNI only
+  a1 <- 1.143349
+  a2 <- -0.1015468
+  a3 <- 1.6284317
 
-  # Response to Fertilization
-  b1<- 0.375379
-  b2 <- 0.652099
-  b3 <- 0.154525
-  b4 <- 0.100104
-
+  # UNI + IMP
+  #a1 <- 1.143921
+  #a2 <- -0.102330
+  #a3 <- 1.668200
 
   # 1. If all informations are provided
   if(is.na(HDOM)==F && is.na(SI)==F && is.na(AGE)==F){
     stop("Warning - No parameteres to estimated. You already have all information.")
   }
 
-  # 2. If HDOM is missing and there is no fertilization
-  else if(is.na(HDOM)==T && is.na(SI)==F && is.na(AGE)==F && is.na(AGEFERT)==T){
-    HDOM <- a1*SI*(1-exp(a2*AGE))^a3 + (a4*Z1 + a5*Z2 + a6*Z3 + a7*Z1*Z3)*AGE*exp(a8*AGE)
+  # 2. If HDOM is missing
+  else if(is.na(HDOM)==T && is.na(SI)==F && is.na(AGE)==F){
+    HDOM <- a1*SI*(1-exp(a2*AGE))^a3
   }
 
-  # 3. If HDOM is missing and there is fertilization
-  else if(is.na(HDOM)==T && is.na(SI)==F && is.na(AGE)==F && is.na(AGEFERT)==F){
-    HDOM <- a1*SI*(1-exp(a2*AGE))^a3 + (a4*Z1 + a5*Z2 + a6*Z3 + a7*Z1*Z3)*AGE*exp(a8*AGE)
-    HDOM <- HDOM + (b1*T1 + b2*T2)*(AGE-AGEFERT)*exp(-(AGE-AGEFERT)*(b3*T1 + b4*T2))
-  }
-
-  # 4. If SI is missing and there is no fertilization
+  # 3. If SI is missing and there is no fertilization
   else if(is.na(HDOM)==F && is.na(SI)==T && is.na(AGE)==F && is.na(AGEFERT)==T){
     #SI <- (HDOM)/(a1*(1-exp(a2*AGE))^a3)
-    SI <- (HDOM - (a4*Z1 + a5*Z2 + a6*Z3 + a7*Z1*Z3)*AGE*exp(a8*AGE))/(a1*(1-exp(a2*AGE))^a3)
+    SI <- (HDOM)/(a1*(1-exp(a2*AGE))^a3)
   }
 
 
@@ -83,7 +67,7 @@ stand.SITE  <-  function(HDOM=NA, SI=NA, AGE=NA, Z1=0, Z2=0, Z3=0, T1=0, T2=0, A
   else if(is.na(AGE)==F | is.na(HDOM)==T && is.na(AGEFERT)==T ){
     stop("Warning - Please provide information required.")
   }
-  
+
   return(list(HDOM=HDOM, SI=SI))
 
 }
